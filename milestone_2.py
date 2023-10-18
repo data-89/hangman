03567a77bb6d9114d5d8082f517039a7137f47bf
data-89 <145280164+data-89@users.noreply.github.com>
2023-10-18 20:46:50 +0100

    Update milestone_2.py
    add validation of user input

--- milestone_2.py
+++ milestone_2.py
@@ -2,3 +2,9 @@ import random
 word_list = ['banana', 'watermelon', 'apricot', 'peach', 'plum']
 word = random.choice(word_list)
 print(word)
+
+guess = input("Enter a single alphabetical character: ")
+if len(guess) == 1 and guess.isalpha()
+    print("Good guess!")
+else:
+    print("Oops! That is not a valid input.")
