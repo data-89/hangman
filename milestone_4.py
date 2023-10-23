313e7566c89150a2f5935e0579579920c14642ed
rafal <rafal@MacBook-Air-rafal-2.local>
2023-10-23 12:00:04 +0100

    Fixes issue of printing the same text twice.

--- milestone_4.py
+++ milestone_4.py
@@ -1,5 +1,4 @@
 import random
-word_list = ['banana', 'watermelon', 'apricot', 'peach', 'plum']
 word = random.choice(word_list)
 
 class Hangman:
@@ -15,28 +14,28 @@ class Hangman:
         if self.guess in self.word:
             print(f"Good guess! {self.guess} is in the word.")
         else:
-            self.num_lives -= 1
             print(f"Sorry, {self.guess} is not in the word. Try again.")
-            print(f"You have {self.num_lives} lives left.")
-
 
     def ask_for_input(self):
-        while True:
-            self.guess = input("Enter a single alphabetical character: ")
-            if len(self.guess) == 1 and self.guess.isalpha():
-                self.guess = self.guess.lower()
-                self.check_guess()
-                while self.num_lives != 0:
-                    if self.guess in self.word:
-                        for i in range(len(self.word)):
-                            if self.word[i] == self.guess:
-                                self.word_guessed[i] = self.guess
-                                self.list_of_guesses.append(self.guess)
-                                print(f"Good guess! '{self.guess}' is in the word.")
-                    else self.guess in self.list_of_guesses:
-                        print(f"You already tried that letter - '{self.guess}'. Try a different letter.")
+        while self.num_lives != 0:
+            guess = input("Enter a single alphabetical character: ")
+            if len(guess) == 1 and guess.isalpha():
+                self.guess = guess.lower()
+                self.check_guess(guess)
+                if self.guess in self.word:
+                    for i in range(len(self.word)):
+                        if self.word[i] == self.guess:
+                            self.word_guessed[i] = self.guess
+                            self.list_of_guesses.append(self.guess)
+                elif self.guess in self.list_of_guesses:
+                     print(f"You already tried that letter - '{self.guess}'. Try a different letter.")
+                else:
+                    self.num_lives -= 1
             else:
-                print("Invalid letter. Please, enter a single alphabetical character.")      
+                print("Invalid letter. Please, enter a single alphabetical character.")   
+        if self.num_lives == 0:
+            print("Game over. The word was:", self.word)
 
-my_hangman = Hangman()
-Hangman.ask_for_input()
+word_list = ['banana', 'watermelon', 'apricot', 'peach', 'plum']
+my_hangman = Hangman(word_list)
+print(my_hangman.ask_for_input())
